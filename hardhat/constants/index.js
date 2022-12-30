466eeba2c715871c09d31833b73773a742c89615
AtrikDas <atrikdas@gmail.com>
2022-12-30 23:31:47 +0800

    initial commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x863017fA500AcCaD128554bC8f632f1d5885D7fB";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
